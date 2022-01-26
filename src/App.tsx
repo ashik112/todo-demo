23676937dfc1d74f6d5a88456ec772066df7fd02
Ashik <ashekur.rahman@goama.com>
2022-01-26 18:45:02 +0600

    demo view

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        TODOS
+        <input type="text" />
+        <button type="button">Save</button>
+        <div>
+          <ul>
+            <li>
+              Complete assignment: Jan 26,2022 18:42
+              <button type="button">Delete</button>
+              <button type="button">Edit</button>
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
   );
